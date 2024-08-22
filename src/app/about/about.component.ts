152ea5d71dde7ed76a314be62defcd862d4fe679
pcyuen98 <pcyuen98@gmail.com>
2024-08-22 15:06:21 +0800

    updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from '../header/header.component';
 import { GlobalConstants } from '../../environments/GlobalConstants';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -12,12 +13,23 @@ import { GlobalConstants } from '../../environments/GlobalConstants';
 })
 
 
-export class AboutComponent  implements OnInit {
+export class AboutComponent implements OnInit {
 
-  constructor() { console.log("info....")}
+  constructor(public sanitizer: DomSanitizer,) { console.log("info....") }
 
-  ngOnInit() {GlobalConstants.isAbout = true}
+  ngOnInit() { GlobalConstants.isAbout = true }
 
-
+  features = "http://219.93.129.18:9000/app/pic/features.png"
+  how = "http://219.93.129.18:9000/app/pic/how.png"
+  bible_cn = "http://219.93.129.18:9000/app/pic/bible_cn.png"
+  bible_en = "http://219.93.129.18:9000/app/pic/bible_en.png"
   filtered_ads = "https://eyebot.name.my/temp/kkm.jpg"
+
+  getChinese() {
+    return GlobalConstants.isChinese
+  }
+
+  getisGospel() {
+    return GlobalConstants.isGospelNews
+  }
 }
